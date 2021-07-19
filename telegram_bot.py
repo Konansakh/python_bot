0f83ca174c9cdd2ecc54fc334d913ef421da4826
Конаныхин Виктор Анатольевич <vkonanikhin@cinimex.ru>
2021-07-19 13:32:02 +0300

    First telegram bot

---
+++ telegram_bot.py
@@ -0,0 +1,37 @@
+import logging
+import settings
+from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters # импортируем из папки telegram/ext файл Updater.py и обработчик команд
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def start_bot(update: Updater, context:CallbackContext):
+    print(update)
+    mytext = """Hello {}
+    
+    I have only /start command!""".format(update.message.chat.first_name)
+    print("Hello")
+    update.message.reply_text(mytext)
+
+def chat(update: Updater, context:CallbackContext):
+    text = update.message.text
+    logging.info(text)
+    
+    update.message.reply_text(text)
+
+def main():
+    updtr = Updater(settings.TOKEN_TELEGRAM) # указываем токен
+    
+    updtr.dispatcher.add_handler(CommandHandler("start", start_bot)) # по команде start будет вызываться функция start_bot
+    updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat)) 
+    
+    
+    updtr.start_polling() # подключаемся к платформе
+    updtr.idle()
+
+if __name__ == "__main__":
+    logging.info('Bot started')
+    main()                      # конструкция для запуска функции main из текущего файла, а не main импорта
+    
